d97ac5051f8ecdb9ecfcef05de568cd969d08e6a
russell <raboley@gmail.com>
2024-09-21 13:23:28 -0700

    Start and Stop recording hooks set up

---
+++ KeyScripter/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+
+namespace KeyScripter
+{
+    public partial class MainWindow : Window
+    {
+        private bool isRecording = false;
+        private RecordKeyboard _recordKeyboard = new RecordKeyboard();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RecordButton_Click(object sender, RoutedEventArgs e)
+        {
+            isRecording = !isRecording;
+            if (isRecording)
+            {
+                RecordButton.Content = "■ Stop";
+                StartRecording();
+            }
+            else
+            {
+                RecordButton.Content = "● Record";
+                string output = StopRecording();
+                OutputTextBox.Text = output;
+            }
+        }
+
+        private void StartRecording(bool start = true)
+        {
+            _recordKeyboard.StartRecording();
+        }
+        
+        private string StopRecording()
+        {
+            return _recordKeyboard.StopRecording();
+        }
+    }
+}
---
+++ KeyScripter/RecordKeyboard.cs
@@ -0,0 +1,17 @@
+﻿namespace KeyScripter;
+
+public class RecordKeyboard
+{
+    // start recording
+    // stop recording
+    // write out recording
+    public void StartRecording()
+    {
+        
+    }
+
+    public string StopRecording()
+    {
+        return "some string";
+    }
+}
