5430dfd9b05e24e742827ea63a4dc5cdc4d67dbd
russell <raboley@gmail.com>
2024-09-22 13:25:56 -0700

    It works! it can now write text to notepad~!

---
+++ KeyScripter/PlaybackKeyboard.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Input;
+
+namespace KeyScripter
+{
+    public class PlaybackKeyboard
+    {
+        public void Play(List<KeyEvent> keyEvents)
+        {
+            foreach (var keyEvent in keyEvents)
+            {
+                if (keyEvent.EventType == KeyEventType.KeyDown)
+                {
+                    KeyDown(keyEvent.Key);
+                }
+                else if (keyEvent.EventType == KeyEventType.KeyUp)
+                {
+                    KeyUp(keyEvent.Key);
+                }
+
+                if (keyEvent.Timestamp > 0)
+                {
+                    Thread.Sleep((int)keyEvent.Timestamp);
+                }
+            }
+        }
+
+        private void KeyDown(Key key)
+        {
+            var keyCode = KeyInterop.VirtualKeyFromKey(key);
+            keybd_event((byte)keyCode, 0, 0, 0);
+        }
+
+        private void KeyUp(Key key)
+        {
+            var keyCode = KeyInterop.VirtualKeyFromKey(key);
+            keybd_event((byte)keyCode, 0, KEYEVENTF_KEYUP, 0);
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+        private const uint KEYEVENTF_KEYUP = 0x0002;
+    }
+}
---
+++ KeyToCodeTests/PlaybackKeyboardCodeTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+using KeyScripter;
+using KeyToCode;
+using Xunit;
+
+namespace KeyScripterTests;
+
+public class PlaybackKeyboardCodeTests
+{
+    private readonly Process _notepadProcess;
+    private string _testFileFullPath_TypesKeysIntoNotepad = @"C:\Temp\TypesKeysIntoNotepad.txt";
+    private readonly PlaybackKeyboardCode _keyboard;
+
+    public PlaybackKeyboardCodeTests()
+    {
+        
+        _notepadProcess = new Process {StartInfo = {FileName = "notepad.exe"}};
+        ProcessStartInfo psi = new ProcessStartInfo("Notepad.Exe", _testFileFullPath_TypesKeysIntoNotepad);
+        _notepadProcess.StartInfo = psi;
+         _keyboard = new PlaybackKeyboardCode();
+
+    }
+    
+    [Fact]
+    public void PlaybackKeyboardCode_Play_TypesKeysIntoNotepad()
+    {
+        // Arrange
+        // create test file with no content
+        File.WriteAllText(_testFileFullPath_TypesKeysIntoNotepad, string.Empty);
+        var expected = "ab";
+        _notepadProcess.Start();
+        _notepadProcess.WaitForInputIdle();
+        _keyboard.Connect(_notepadProcess.MainWindowHandle);
+        
+        // Act
+        _keyboard.KeyDown(VKeys.KEY_A);
+        _keyboard.Sleep(50);
+        _keyboard.KeyUp(VKeys.KEY_A);
+        _keyboard.Sleep(50);
+        _keyboard.KeyDown(VKeys.KEY_B);
+        _keyboard.Sleep(50);
+        _keyboard.KeyUp(VKeys.KEY_B);
+        _keyboard.Sleep(50);
+        _keyboard.KeyDown(VKeys.KEY_LCONTROL);
+        _keyboard.Sleep(50);
+        _keyboard.KeyDown(VKeys.KEY_S);
+        _keyboard.Sleep(50);
+        _keyboard.KeyUp(VKeys.KEY_S);
+        _keyboard.Sleep(50);
+        _keyboard.KeyUp(VKeys.KEY_LCONTROL);
+        _keyboard.Sleep(50);
+
+        // Assert
+        // Save the file
+        
+        
+        _notepadProcess.CloseMainWindow();
+        _notepadProcess.Kill();
+        _notepadProcess.WaitForExit();
+        
+        var actual = File.ReadAllText(_testFileFullPath_TypesKeysIntoNotepad);
+        // reset the file
+        Assert.Equal(expected, actual);
+    }
+    
+}
