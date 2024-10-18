d7e1633cf42b311f84b307e90df7ede5a3db2be9
russell <raboley@gmail.com>
2024-10-18 12:58:49 -0700

    remove zombie code recolor the elements when running.

--- KeyToCodeRecorder/MainWindow.xaml.cs
+++ KeyToCodeRecorder/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Runtime.CompilerServices;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Media;
 using Microsoft.Win32;
 
 namespace KeyScripter;
@@ -274,25 +275,22 @@ private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         RecordButton_Click(this, new RoutedEventArgs());
     }
 
-    private void RecordButton_Click(object sender, RoutedEventArgs e)
+private void RecordButton_Click(object sender, RoutedEventArgs e)
+{
+    _isRecording = !_isRecording;
+    if (_isRecording)
     {
-        _isRecording = !_isRecording;
-        if (_isRecording)
-        {
-            RecordButton.Content = "■ Stop";
-            StartRecording();
-        }
-        else
-        {
-            RecordButton.Content = "● Record";
-            var output = StopRecording();
-            OutputTextBox.Text = output;
-            if (_config.AutomaticallyCopyOutputToClipboard)
-            {
-                Clipboard.SetText(output);
-            }
-        }
+        RecordButton.Content = "■ Stop";
+        RecordButton.Background = new SolidColorBrush(Colors.Tomato); // Lighter hue for stop mode
+        // Start recording logic
     }
+    else
+    {
+        RecordButton.Content = "● Record";
+        RecordButton.Background = new SolidColorBrush(Colors.Red); // Normal color
+        // Stop recording logic
+    }
+}
 
     private void CopyButton_Click(object sender, RoutedEventArgs e)
     {
@@ -301,59 +299,26 @@ private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
 
     private bool _isPlaying = false;
 
-    private async void PlayButton_Click(object sender, RoutedEventArgs e)
+private async void PlayButton_Click(object sender, RoutedEventArgs e)
+{
+    _isPlaying = !_isPlaying;
+    if (_isPlaying)
     {
-        _isPlaying = !_isPlaying;
-        if (_isPlaying)
-        {
-            PlayButton.Content = "■ Stop";
-            var inputKeys = OutputTextBox.Text;
-            await Task.Run(() => _playbackKeyboard.Play(inputKeys));
-            _isPlaying = false;
-            PlayButton.Content = "▶ Play";
-        }
-        else
-        {
-            _playbackKeyboard.Stop();
-            PlayButton.Content = "▶ Play";
-        }
+        PlayButton.Content = "■ Stop";
+        PlayButton.Background = new SolidColorBrush(Colors.MediumSeaGreen); // Lighter hue for stop mode
+        var inputKeys = OutputTextBox.Text;
+        await Task.Run(() => _playbackKeyboard.Play(inputKeys));
+        _isPlaying = false;
+        PlayButton.Content = "▶ Play";
+        PlayButton.Background = new SolidColorBrush(Colors.Green); // Normal color
     }
-
-    private void StartRecording()
+    else
     {
-        _playbackKeyboard.SetForegroundWindow();
-        _recordKeyboard.StartRecording();
+        _playbackKeyboard.Stop();
+        PlayButton.Content = "▶ Play";
+        PlayButton.Background = new SolidColorBrush(Colors.Green); // Normal color
     }
-
-    private string StopRecording()
-    {
-        return _recordKeyboard.StopRecording();
-    }
-
-    private List<KeyEvent> ParseKeyEvents(string input)
-    {
-        var keyEvents = new List<KeyEvent>();
-        var lines = input.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
-
-        foreach (var line in lines)
-        {
-            var parts = line.Split(' ');
-            if (parts.Length < 4) continue;
-
-            var eventType = (KeyEventType)Enum.Parse(typeof(KeyEventType), parts[0]);
-            var key = (VKey)Enum.Parse(typeof(VKey), parts[1]);
-            var timestamp = long.Parse(parts[3].Replace("ms", ""));
-
-            keyEvents.Add(new KeyEvent
-            {
-                EventType = eventType,
-                Key = key,
-                Timestamp = timestamp
-            });
-        }
-
-        return keyEvents;
-    }
+}
 
     private void SaveSelectedProcess(string processName)
     {
