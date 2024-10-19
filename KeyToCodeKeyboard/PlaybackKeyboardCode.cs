3a3ff443924a1e595f3122e8e21d78054392d7a1
russell <raboley@gmail.com>
2024-10-19 15:29:08 -0700

    fix

--- KeyToCodeKeyboard/PlaybackKeyboardCode.cs
+++ KeyToCodeKeyboard/PlaybackKeyboardCode.cs
@@ -1,22 +1,41 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.CodeAnalysis.Scripting;
 
 namespace KeyToCodeKeyboard;
 
-public class PlaybackKeyboardCode
+/// <summary>
+/// Class responsible for playing back keyboard input.
+/// </summary>
+public class PlaybackKeyboardCode : IDisposable
 {
     private readonly WindowHelper _windowHelper;
     private CancellationTokenSource _cancellationTokenSource;
-    public CancellationToken CancellationToken { get; set; }
+    public CancellationToken CancellationToken { get; private set; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PlaybackKeyboardCode"/> class.
+    /// </summary>
+    /// <param name="windowHelper">The window helper instance.</param>
     public PlaybackKeyboardCode(WindowHelper windowHelper)
     {
-        _windowHelper = windowHelper;
+        _windowHelper = windowHelper ?? throw new ArgumentNullException(nameof(windowHelper));
     }
 
+    /// <summary>
+    /// Plays the specified input keys.
+    /// </summary>
+    /// <param name="inputKeys">The input keys to play.</param>
     public async Task Play(string inputKeys)
     {
+        if (inputKeys == null) throw new ArgumentNullException(nameof(inputKeys));
+
         _cancellationTokenSource = new CancellationTokenSource();
         var cancellationToken = _cancellationTokenSource.Token;
         CancellationToken = cancellationToken;
@@ -26,19 +45,19 @@ public class PlaybackKeyboardCode
 
         var scriptOptions = ScriptOptions.Default
             .AddReferences(assemblies)
-            .AddImports("System", "System.Threading", "KeyToCode");
+            .AddImports("System", "System.Threading", "KeyToCode", "KeyToCodeKeyboard");
 
         var script = CSharpScript.Create(inputKeys, scriptOptions, typeof(Globals));
-        var globals = new Globals { _keyboard = this};
+        var globals = new Globals { _keyboard = this };
 
         try
         {
-            await script.RunAsync(globals, cancellationToken);
+            await script.RunAsync(globals, cancellationToken).ConfigureAwait(false);
         }
         catch (OperationCanceledException)
         {
             Console.WriteLine("Script playback was cancelled");
-            // ensure all keys that were pressed are released
+            // Ensure all keys that were pressed are released
             foreach (var key in Enum.GetValues(typeof(VKey)).Cast<VKey>())
             {
                 KeyUp(key);
@@ -46,17 +65,28 @@ public class PlaybackKeyboardCode
         }
     }
 
+    /// <summary>
+    /// Stops the playback.
+    /// </summary>
     public void Stop()
     {
         _cancellationTokenSource?.Cancel();
     }
 
+    /// <summary>
+    /// Connects to the specified process main window handle.
+    /// </summary>
+    /// <param name="processMainWindowHandle">The process main window handle.</param>
     public void Connect(IntPtr processMainWindowHandle)
     {
         _windowHelper.Connect(processMainWindowHandle);
     }
 
-    public void Sleep(int milliseconds)
+    /// <summary>
+    /// Sleeps for the specified number of milliseconds, is cancellable.
+    /// </summary>
+    /// <param name="milliseconds">The number of milliseconds to sleep.</param>
+    public async Task Sleep(int milliseconds)
     {
         var delayTask = Task.Delay(milliseconds, CancellationToken);
         try
@@ -70,6 +100,11 @@ public class PlaybackKeyboardCode
         }
     }
 
+    /// <summary>
+    /// Simulates a key down event.
+    /// </summary>
+    /// <param name="key">The key to press down.</param>
+    /// <returns>True if the key down event was successful; otherwise, false.</returns>
     public bool KeyDown(VKey key)
     {
         if (!_windowHelper.SetForegroundWindow())
@@ -79,11 +114,20 @@ public class PlaybackKeyboardCode
         return _windowHelper.PostMessage(_windowHelper.ProcessMainWindowHandle, Message.KeyDown, key);
     }
 
+    /// <summary>
+    /// Sets the foreground window.
+    /// </summary>
+    /// <returns>True if the foreground window was set; otherwise, false.</returns>
     public bool SetForegroundWindow()
     {
         return _windowHelper.SetForegroundWindow();
     }
 
+    /// <summary>
+    /// Simulates a key up event.
+    /// </summary>
+    /// <param name="key">The key to release.</param>
+    /// <returns>True if the key up event was successful; otherwise, false.</returns>
     public bool KeyUp(VKey key)
     {
         if (!_windowHelper.SetForegroundWindow())
@@ -95,6 +139,11 @@ public class PlaybackKeyboardCode
         return worked;
     }
 
+    /// <summary>
+    /// Simulates a foreground key down event.
+    /// </summary>
+    /// <param name="key">The key to press down.</param>
+    /// <returns>True if the key down event was successful; otherwise, false.</returns>
     public static bool ForegroundKeyDown(VKey key)
     {
         uint intReturn;
@@ -108,6 +157,11 @@ public class PlaybackKeyboardCode
         return intReturn == 0;
     }
 
+    /// <summary>
+    /// Simulates a foreground key up event.
+    /// </summary>
+    /// <param name="key">The key to release.</param>
+    /// <returns>True if the key up event was successful; otherwise, false.</returns>
     public static bool ForegroundKeyUp(VKey key)
     {
         uint intReturn;
@@ -126,4 +180,12 @@ public class PlaybackKeyboardCode
 
     private const int InputKeyboard = 1;
     private const uint KeyeventfKeyup = 0x0002;
+
+    /// <summary>
+    /// Disposes the resources used by the <see cref="PlaybackKeyboardCode"/> class.
+    /// </summary>
+    public void Dispose()
+    {
+        _cancellationTokenSource?.Dispose();
+    }
 }
